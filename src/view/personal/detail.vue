9d856199dd8f1fb6d548f4f7cb6a3723aac646b7
郑佳俊 <2495893564@qq.com>
2021-06-25 18:19:12 +0800

    权限

---
+++ src/view/personal/detail.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="detail">
+      detail
+  </div>
+</template>
+
+<script lang="ts">
+import {defineComponent} from "vue"
+export default defineComponent ({
+
+})
+</script>
+
+<style>
+
+</style>
